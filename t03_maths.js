51943b4b3788eb9d1bc7c65d370b56ad5efd8335
MankayFan <24449ds@hvhs.school.nz>
2026-04-23 02:46:21 +0000

    Apr 23, 2026, 2:46 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,20 @@
-/* Paste your code from task 2 here */
+/****************************
+THIS IS NOT A BLOCK COMMENT
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+****************************/
+console.log("not running at all")
+console.log("LAlalalalalalalal")
+
+// Variable
+
+/*******************************************
+ MAIN CODEE 
+ ******************************************/
+
+var Mrbeastalivecounter = 1;
+console.log("Mr beast is " + Mrbeastalivecounter);
+
+
+/*******************************************
+ FUNCTIONS
+ ******************************************/ 
