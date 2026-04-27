b5ff0a1e63970c77685413eaf0e8c9850b1f4958
MankayFan <24449ds@hvhs.school.nz>
2026-04-27 22:52:32 +0000

    Apr 27, 2026, 10:52 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,30 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+THIS IS NOT A BLOCK COMMENT
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+****************************/
+console.log("not running at all")
+console.log("LAlalalalalalalal")
+
+// Variable
+
+/*******************************************
+ MAIN CODEE 
+ ******************************************/
+
+var Mrbeastalivecounter = 3;
+var Mrbeastdeathcounter = 7;
+var answer;
+
+answer = Mrbeastalivecounter + Mrbeastdeathcounter
+console.log("Mr beast is", Mrbeastalivecounter + Mrbeastdeathcounter);
+answer = Mrbeastalivecounter + Mrbeastdeathcounter
+console.log("Mr beast is", Mrbeastalivecounter - Mrbeastdeathcounter);
+answer = Mrbeastalivecounter + Mrbeastdeathcounter
+console.log("Mr beast is", Mrbeastalivecounter * Mrbeastdeathcounter);
+answer = Mrbeastalivecounter + Mrbeastdeathcounter
+console.log("Mr beast is", Mrbeastalivecounter / Mrbeastdeathcounter);
+
+
+/*******************************************
+ FUNCTIONS
+ ******************************************/ 
