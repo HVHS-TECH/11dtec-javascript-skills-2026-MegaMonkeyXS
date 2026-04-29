e17a68527f611a1c91266146237572c878517465
MankayFan <24449ds@hvhs.school.nz>
2026-04-29 00:51:22 +0000

    Apr 29, 2026, 12:51 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -8,7 +8,7 @@ var width = 10;
 var perimeter = 2 length + 2 * width; 
 
 // calculate the area
-var area = length + width;
+var area = length * width;
 
 // Display the results
 console.log("A area of a piece of paper with a length of + length, and a width of + width is area")
