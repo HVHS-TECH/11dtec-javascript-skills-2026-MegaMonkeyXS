5d4d2d8605558f0287fb7cd2519d8cc7277da731
MankayFan <24449ds@hvhs.school.nz>
2026-04-30 02:39:47 +0000

    Apr 30, 2026, 2:39 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -5,7 +5,7 @@ var length = 8;
 var width = 10;
 
 // calculate the perimeter
-var perimeter = 2 length + 2 * width; 
+var perimeter = length + 2 * width; 
 
 // calculate the area
 var area = length * width;
