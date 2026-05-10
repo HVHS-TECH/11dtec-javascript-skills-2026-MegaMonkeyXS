3732ae3bb5c216274f512897ef35ff5ef484bed3
MankayFan <24449ds@hvhs.school.nz>
2026-05-10 21:39:55 +0000

    May 10, 2026, 9:39 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,36 @@
-/* Paste your code from the last task */
+// THIS IS NOT A BLOCK COMMENT
+// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+console.log("JavaScript is running");
+console.log("LAlalalalalalalal");
+
+/*******************************************
+ MAIN CODE
+ *******************************************/
+
+var Userage = 463674329137270;
+var Currentyear = 1943;
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML += "<p>Also added by JavaScript</p>";
+OUTPUT.innerHTML += "<p>Did I mention this was added by JavaScript</p>";
+OUTPUT.innerHTML += "<p>Yeah this is definitely JavaScript</p>";
+OUTPUT.innerHTML += "<img src='Lemon_John.jpg' alt='John Lemon'>";
+
+writeLine();
+
+/*******************************************
+ FUNCTIONS
+ *******************************************/
+
+function writeLine() {
+  OUTPUT.innerHTML += "<p>Meet The Beatles</p>";
+}
+
+function start() {
+  var answer = Currentyear - Userage;
+
+  OUTPUT.innerHTML += "<p>You are " + answer + " years old</p>";
+}
