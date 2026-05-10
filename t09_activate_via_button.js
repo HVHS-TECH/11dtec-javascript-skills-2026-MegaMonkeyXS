a20cc8eee7b9442f34c3f24574a1b04100dfd2c3
MankayFan <24449ds@hvhs.school.nz>
2026-05-10 21:28:58 +0000

    May 10, 2026, 9:28 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,40 +1,36 @@
 // THIS IS NOT A BLOCK COMMENT
 // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
-console.log("not running at all");
+console.log("JavaScript is running");
 console.log("LAlalalalalalalal");
-
 
 /*******************************************
  MAIN CODE
- ******************************************/
+ *******************************************/
 
- var Userage = 65329230567;
- var Currentyear = 2026
-
+var Userage = 463674329137270;
+var Currentyear = 1943;
 
 const OUTPUT = document.getElementById("JavaScriptOutput");
 
 OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 OUTPUT.innerHTML += "<p>Also added by JavaScript</p>";
 OUTPUT.innerHTML += "<p>Did I mention this was added by JavaScript</p>";
-OUTPUT.innerHTML += "<p>Yeah this is definitely Javascript</p>";
+OUTPUT.innerHTML += "<p>Yeah this is definitely JavaScript</p>";
 OUTPUT.innerHTML += "<img src='Lemon_John.jpg' alt='John Lemon'>";
 
-writeLine()
+writeLine();
 
 /*******************************************
  FUNCTIONS
- ******************************************/
+ *******************************************/
 
 function writeLine() {
   OUTPUT.innerHTML += "<p>Meet The Beatles</p>";
 }
 
 function start() {
-JavaScriptOutput.innerHTML
-JavaScriptOutput.innerHTML
-answer = Currentyear - Userage;
-JavaScriptOutput.innerHTML
+  var answer = Currentyear - Userage;
 
+  OUTPUT.innerHTML += "<p>You are " + answer +"</p>";
 }
