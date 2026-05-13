fc01346d91ff3f134e8abfb8ca07549aae4d03c9
MankayFan <24449ds@hvhs.school.nz>
2026-05-13 00:56:16 +0000

    May 13, 2026, 12:56 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -6,49 +6,49 @@ console.log("LAlalalalalalalal");
 
 /*******************************************
  MAIN CODE
- *******************************************/
+  *******************************************/
 
-var Currentyear = 1943;
+  var Currentyear = 1943;
 
-const OUTPUT = document.getElementById("JavaScriptOutput");
-let userName = "hello";
-let userAge = "hello";
-let userDollar = "hello";
+  const OUTPUT = document.getElementById("JavaScriptOutput");
+  let userName = "hello";
+  let userAge = "hello";
+  let userDollar = "hello";
 
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
-OUTPUT.innerHTML += "<p>Also added by JavaScript</p>";
-OUTPUT.innerHTML += "<p>Did I mention this was added by JavaScript</p>";
-OUTPUT.innerHTML += "<p>Yeah this is definitely JavaScript</p>";
-OUTPUT.innerHTML += "<img src='Lemon_John.jpg' alt='John Lemon'>";
+  OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+  OUTPUT.innerHTML += "<p>Also added by JavaScript</p>";
+  OUTPUT.innerHTML += "<p>Did I mention this was added by JavaScript</p>";
+  OUTPUT.innerHTML += "<p>Yeah this is definitely JavaScript</p>";
+  OUTPUT.innerHTML += "<img src='Lemon_John.jpg' alt='John Lemon'>";
 
-writeLine();
-
-
-
-/*******************************************
- FUNCTIONS
- *******************************************/
-
-function writeLine() {
-  OUTPUT.innerHTML += "<p>Meet The Beatles</p>";
-}
-
-function start() {
-  var answer = Currentyear - Userage;
-  OUTPUT.innerHTML += "<p>You are " + answer + " years old</p>";
-}
+  writeLine();
 
 
 
-function getFormInput() {
-const NAME_FIELD = document.getElementById("Namefield");
-const AGE_FIELD = document.getElementById("Agefield");
-const DOLLAR_FIELD = document.getElementById("Dollarfield");
-userName= NAME_FIELD.value;
-userAge= AGE_FIELD.value;
-userDollar= DOLLAR_FIELD.value;
-OUTPUT.innerHTML += "<p>Welcome to the website " + userName + "</p>";   
-OUTPUT.innerHTML += "<p>You are " + userAge + " years old</p>"; 
-OUTPUT.innerHTML += "<p>You have " + userDollar + " yen</p>"; 
+  /*******************************************
+   FUNCTIONS
+    *******************************************/
 
-}
+    function writeLine() {
+      OUTPUT.innerHTML += "<p>Meet The Beatles</p>";
+      }
+
+      function start() {
+        var answer = Currentyear - Userage;
+          OUTPUT.innerHTML += "<p>You are " + answer + " years old</p>";
+          }
+
+
+
+          function getFormInput() {
+          const NAME_FIELD = document.getElementById("Namefield");
+          const AGE_FIELD = document.getElementById("Agefield");
+          const DOLLAR_FIELD = document.getElementById("Dollarfield");
+          userName= NAME_FIELD.value;
+          userAge= AGE_FIELD.value;
+          userDollar= DOLLAR_FIELD.value;
+          OUTPUT.innerHTML += "<p>Welcome to the website " + userName + "</p>";   
+          OUTPUT.innerHTML += "<p>You are " + userAge + " years old</p>"; 
+          OUTPUT.innerHTML += "<p>You have " + userDollar + " yen</p>"; 
+
+          }
--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,54 @@
-/* Paste your code from the last task */
+// THIS IS NOT A BLOCK COMMENT
+// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+console.log("JavaScript is running");
+console.log("LAlalalalalalalal");
+
+/*******************************************
+ MAIN CODE
+ *******************************************/
+
+var Currentyear = 1943;
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+let userName = "hello";
+let userAge = "hello";
+let userDollar = "hello";
+
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML += "<p>Also added by JavaScript</p>";
+OUTPUT.innerHTML += "<p>Did I mention this was added by JavaScript</p>";
+OUTPUT.innerHTML += "<p>Yeah this is definitely JavaScript</p>";
+OUTPUT.innerHTML += "<img src='Lemon_John.jpg' alt='John Lemon'>";
+
+writeLine();
+
+
+
+/*******************************************
+ FUNCTIONS
+ *******************************************/
+
+function writeLine() {
+  OUTPUT.innerHTML += "<p>Meet The Beatles</p>";
+}
+
+function start() {
+  var answer = Currentyear - Userage;
+  OUTPUT.innerHTML += "<p>You are " + answer + " years old</p>";
+}
+
+
+
+function getFormInput() {
+const NAME_FIELD = document.getElementById("Namefield");
+const AGE_FIELD = document.getElementById("Agefield");
+const DOLLAR_FIELD = document.getElementById("Dollarfield");
+userName= NAME_FIELD.value;
+userAge= AGE_FIELD.value;
+userDollar= DOLLAR_FIELD.value;
+OUTPUT.innerHTML += "<p>Welcome to the website " + userName + "</p>";   
+OUTPUT.innerHTML += "<p>You are " + userAge + " years old</p>"; 
+OUTPUT.innerHTML += "<p>You have " + userDollar + " yen</p>"; 
+
+}
